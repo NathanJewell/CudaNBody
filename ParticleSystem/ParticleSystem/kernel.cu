e68d0dc1b5d4c8b25f4a696febc142d29594858b
Nathan Jewell <NJewell5880@student.pps.net>
2016-05-02 11:17:49 -0700

    Works but not on this project

--- ParticleSystem/ParticleSystem/kernel.cu
+++ ParticleSystem/ParticleSystem/kernel.cu
@@ -1,71 +1,91 @@
 
-#include "cuda_runtime.h"
+#include <cuda_runtime.h>
 #include "device_launch_parameters.h"
 #include <iostream>
 #include <stdio.h>
 #include <chrono>
 typedef std::chrono::high_resolution_clock Clock;
 
-//defining program attributes
-static const int N = 512*512;
-static const size_t size = N * sizeof(int);
-static const int TPB = 512;	//threads per block
 
-__global__ void ARR_ADD(int* res, int* in1, int *in2, int n)
+
+__global__ void ARR_ADD(float* res, const float* in1, const float *in2, int n)
 {
-	int index = threadIdx.x + blockIdx.x * blockDim.x;
+	int index = blockDim.x * blockIdx.x + threadIdx.x;
 	if (index < n)
-		res[index] = in1[index] + in2[index];
+	{
+		res[index] = sqrt(in1[index] * in2[index]);
+	}
+
 }
 
 int main()
 {
-	
-
+	//defining program attributes
+	int N = 10000000;
+	size_t size = N * sizeof(float);
+	int TPB = 1024;	//threads per block
+	int times = 100;
 
 
 	//allocating host memory
-	int *res = (int*) malloc(size);
-	int *in1 = (int*) malloc(size);
-	int *in2 = (int*) malloc(size);
+
+
+	float *res = (float *)malloc(size);
+	float *in1 = (float *)malloc(size);
+	float *in2 = (float *)malloc(size);
 
 	//defining pointers to device memory 
-	int *d_res, *d_in1, *d_in2;
-
-	cudaMalloc(&d_res, size);
-	cudaMalloc(&d_in1, size);
-	cudaMalloc(&d_in2, size);
+	float *d_res = NULL;
+	float *d_in1 = NULL;
+	float *d_in2 = NULL;
 
 	//initializing values
 	for (int i = 0; i < N; i++)
 	{
-		in1[i] = i + 1;
-		in2[i] = i + 2;
+		in1[i] = rand() / (float)RAND_MAX;
+		in2[i] = rand() / (float)RAND_MAX;
 	}
-
-
+	auto t1 = Clock::now();
+	cudaMalloc((void**)&d_res, size);
+	cudaMalloc((void**)&d_in1, size);
+	cudaMalloc((void**)&d_in2, size);
 	//copying to device memory
-	cudaMemcpy(d_res, res, size, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_in1, in1, size, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_in2, in2, size, cudaMemcpyHostToDevice);
+	cudaError_t err = cudaMemcpy(d_res, res, size, cudaMemcpyHostToDevice);
+	err = cudaMemcpy(d_in1, in1, size, cudaMemcpyHostToDevice);
+	err = cudaMemcpy(d_in2, in2, size, cudaMemcpyHostToDevice);
 
 	//call function to execute on device
-	auto t1 = Clock::now();
-	ARR_ADD << <N/TPB, TPB>> >(d_res, d_in1, d_in2, N);
-	auto t2 = Clock::now();
-	std::cout << "Delta t2-t1: "
-		<< std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()
-		<< " nanoseconds" << std::endl;
+
+	int numblocks = (N + TPB - 1) / TPB;
+	for (int i = 0; i < times; i++)
+	{
+		ARR_ADD << <numblocks, TPB >> >(d_res, d_in1, d_in2, N);
+	}
+
+	err = cudaGetLastError();
+
+	if (err != cudaSuccess)
+	{
+		fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n", cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+
 	//copy result back to host
-	cudaMemcpy(res, d_res, size, cudaMemcpyDeviceToHost);
+
+	err = cudaMemcpy(res, d_res, size, cudaMemcpyDeviceToHost);
 
 	//free memory on device
 	cudaFree(d_res);
 	cudaFree(d_in1);
 	cudaFree(d_in2);
 
+	auto t2 = Clock::now();
+	std::cout << "Delta t2-t1: "
+		<< std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()
+		<< " nanoseconds" << std::endl;
+
 	//print result to window
-	long int checksum = 0;
+	double checksum = 0;
 	for (int i = 0; i < N; i++)
 	{
 		checksum += res[i];
@@ -73,27 +93,33 @@ int main()
 
 
 	std::cout << "Parrelelized N=" << N << " : " << checksum << std::endl;
-
+	float *in1N = in2;
+	float *in2N = in1;
 	//free host memory
-	delete res;
-	delete in1;
-	delete in2;
+	free(res);
 
-	int resN[N], in1N[N], in2N[N];
-	for (int i = 0; i < N; i++)
-	{
-		in1N[i] = i + 1;
-		in2N[i] = i + 2;
-	}
+
+
+
 	auto t1N = Clock::now();
-	for (int i = 0; i < N; i++)
+
+	float *resN = (float*)malloc(size);
+
+
+	for (int ii = 0; ii < times; ii++)
 	{
-		resN[i] = in1N[i] + in2N[i];
+		for (int i = 0; i < N; i++)
+		{
+			resN[i] = sqrt(in1N[i] * in2N[i]);
+		}
 	}
+
+
 	auto t2N = Clock::now();
 	std::cout << "Delta t2-t1: "
 		<< std::chrono::duration_cast<std::chrono::nanoseconds>(t2N - t1N).count()
 		<< " nanoseconds" << std::endl;
+
 	checksum = 0;
 	for (int i = 0; i < N; i++)
 	{
@@ -101,5 +127,6 @@ int main()
 	}
 	std::cout << "Standard N=" << N << " : " << checksum << std::endl;
 
-
+	free(in1);
+	free(in2);
 }
